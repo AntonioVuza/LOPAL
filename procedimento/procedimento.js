e7dcc9b9e94c2bff1eb128683def0c7345525d96
AntonioVuza <antoniomarcosoficial33@gmail.com>
2026-03-18 16:50:44 -0300

    feat: Urna e Procedimento

---
+++ procedimento/procedimento.js
@@ -0,0 +1,30 @@
+function procedimento(){
+
+
+    function inicio(){
+        let resultado = calcular(a = 3, b = 4);
+        mensagen(frase = "O resultado de a² + b² é:  " + resultado);
+    }
+
+    function mensagen(frase){
+        let linha = "-"
+        let i = 0;
+       
+
+        do{
+            linha = linha + "-"
+            i++;
+        }while( i < 50 );
+
+        alert( linha + "\n" + frase + "\n" + linha);
+
+    }
+
+    function calcular (a, b){
+        return a * a + b * b;
+      
+    }
+
+    inicio();
+
+}
